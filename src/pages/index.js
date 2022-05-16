3ad03f43c0ca82a904bc89c680df783dfb8d4b04
sjrdevelopment <stuj.robinson@gmail.com>
2022-05-16 17:29:19 +0100

    update styles responsive breakpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,83 +34,100 @@ const IndexPage = () => {
                 Cleancore Intelligence - Smart cleaning compliance
             </h1>
             <div className={styles.banner}>
-                <img className={styles.mainImg} src={logo} alt="" />
-                <p className={styles.subheading}>Smart cleaning compliance</p>
+                <div className={styles.maxWrapper}>
+                    <img className={styles.mainImg} src={logo} alt="" />
+                    <p className={styles.subheading}>
+                        Smart cleaning compliance
+                    </p>
+                </div>
             </div>
             <div className={styles.subHeroWrapper}>
                 <div className={styles.subHero}>
-                    <img
-                        className={styles.subHeroImg}
-                        src={subHeroImg}
-                        alt=""
-                    />
-                    <div className={styles.subHeroInner}>
-                        <p>
-                            Cleancore uses wireless technology to automate the
-                            tracking and alerting of cleaning schedules and
-                            compliance to your standards, replacing the need for
-                            manual record keeping.
-                        </p>
-                        <p>
-                            Build trust in facilities and communicate safety
-                            assurance to building users with digital inspection
-                            displays in shared areas.
-                        </p>
+                    <div className={styles.maxWrapper}>
+                        <img
+                            className={styles.subHeroImg}
+                            src={subHeroImg}
+                            alt=""
+                        />
+                        <div className={styles.subHeroInner}>
+                            <p>
+                                Cleancore uses wireless technology to automate
+                                the tracking and alerting of cleaning schedules
+                                and compliance to your standards, replacing the
+                                need for manual record keeping.
+                            </p>
+                            <p>
+                                Build trust in facilities and communicate safety
+                                assurance to building users with digital
+                                inspection displays in shared areas.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className={styles.contentCore}>
-                <div className={styles.contentCoreInner}>
-                    <h2>CORE</h2>
-                    <p>
-                        Project an air of trust in facilities using our Core
-                        systems
-                    </p>
-                    <p>
-                        Automated data collection at all your key touch points
-                        in the building for traceability of cleaning activity
-                    </p>
+                <div className={styles.maxWrapper}>
+                    <div className={styles.contentCoreInner}>
+                        <h2>CORE</h2>
+                        <p>
+                            Project confidence in cleanliness to building users
+                            using our Core systems
+                        </p>
+                        <p>
+                            Each Core wirelessly collects data at all your key
+                            touch points in the building for traceability of
+                            cleaning activity
+                        </p>
+                    </div>
+                    <img src={alertImg1} alt="" className={styles.alertImg1} />
+                    <img src={alertImg2} alt="" className={styles.alertImg2} />
                 </div>
-                <img src={alertImg1} alt="" className={styles.alertImg1} />
-                <img src={alertImg2} alt="" className={styles.alertImg2} />
             </div>
             <div className={styles.contentReporting}>
-                <img src={gridImg} alt="" className={styles.gridImg} />
-                <div className={styles.contentReportingInner}>
-                    <h2>REPORTING</h2>
-                    <p>
-                        Real time monitoring and optimisation of cleaning
-                        performance and usage levels, automatically collected
-                        from each Core in your location.
-                    </p>
+                <div className={styles.maxWrapper}>
+                    {' '}
+                    <img src={gridImg} alt="" className={styles.gridImg} />
+                    <div className={styles.contentReportingInner}>
+                        <h2>REPORTING</h2>
+                        <p>
+                            Real time monitoring and optimisation of cleaning
+                            performance and usage levels, automatically
+                            collected from each Core in your location.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div className={styles.contentAlerting}>
-                <div className={styles.contentAlertingInner}>
-                    <h2>ALERTING</h2>
-                    <p>
-                        Exception reporting and alerts when pre-defined cleaning
-                        intervals are missed, or usage levels exceed thresholds
-                    </p>
+                <div className={styles.maxWrapper}>
+                    <div className={styles.contentAlertingInner}>
+                        <h2>ALERTING</h2>
+                        <p>
+                            Exception reporting and alerts when pre-defined
+                            cleaning intervals are missed, or usage levels
+                            exceed thresholds
+                        </p>
+                    </div>
+                    <img src={alertImg} alt="" className={styles.alertImg} />
                 </div>
-                <img src={alertImg} alt="" className={styles.alertImg} />
             </div>
             <footer>
-                <p>
-                    We are currently looking to partner with innovative
-                    Facilities Management companies and building managers who
-                    would be interested in collaborating on a pilot.
-                </p>
-                <p>
-                    Please contact{' '}
-                    <a href="mailto:info@cleancoreintelligence.com">
-                        info@cleancoreintelligence.com
-                    </a>{' '}
-                    to find out more.
-                </p>
-                <p className={styles.disclaimer}>
-                    SJR Technology Ltd trading as Cleancore Intelligence
-                </p>
+                <div className={styles.maxWrapper}>
+                    <p>
+                        We are currently looking to partner with innovative
+                        Facilities Management companies and building managers
+                        who would be interested in collaborating on a pilot.
+                    </p>
+                    <p>
+                        Please contact{' '}
+                        <a href="mailto:info@cleancoreintelligence.com">
+                            info@cleancoreintelligence.com
+                        </a>{' '}
+                        to find out more.
+                    </p>
+                    <p className={styles.disclaimer}>
+                        SJR Technology Ltd trading as Cleancore Intelligence
+                    </p>
+                </div>
             </footer>
         </main>
     )
